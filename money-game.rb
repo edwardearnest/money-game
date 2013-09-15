34c1dbcfdd9ce52674d2f3c0886f887fa9318d3c
Edward Earnest <edwardearnest2015@gmail.com>
2013-09-14 19:55:37 -0500

    a hint of code

---
+++ money-game.rb
@@ -0,0 +1,6 @@
+puts "money-game Copyright (C) 2013  Edward Earnest
+This program comes with ABSOLUTELY NO WARRANTY; for details 
+see the file LICENSE. 
+This is free software, and you are welcome to redistribute it
+under certain conditions; again, see the file LICENSE."
+
