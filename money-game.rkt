ad1fe0edfd58112f6281804d50817a2790de844a
Edward Earnest <edwardearnest2015@gmail.com>
2013-09-17 00:21:41 -0500

    It's a RACKET\!

---
+++ money-game.rkt
@@ -0,0 +1,17 @@
+; Created: 2013-09-17
+; Revised: 2013-09-17
+; 
+; TODO MORE HEADER HERE
+#lang racket
+
+(define program-name "money-game")
+(printf
+ "~a Copyright (C) 2013  Edward Earnest
+   This program comes with ABSOLUTELY NO WARRANTY; for details 
+   see the file LICENSE. 
+
+   This is Free Software, and you are welcome to redistribute it
+   under certain conditions; again, see the file LICENSE.~n"
+ program-name)
+
+; TODO unit test to ensure that this string prints OK.?
